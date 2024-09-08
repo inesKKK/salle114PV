9cafd5109e7abd7baaf7b14e7f45ca4eb8be693a
KLIMANN INES <klimann@zaclys.net>
2024-09-08 18:37:37 +0200

    les installations à faire

---
+++ a_installer.sh
@@ -0,0 +1,15 @@
+sudo apt install emacs
+sudo apt install tuareg-mode
+sudo apt install utop
+sudo apt install gcc
+sudo apt install gdb
+sudo apt install postgresql
+
+# pour les TP MP2I
+sudo apt install tree
+sudo apt install net-tools
+sudo apt install gnuplot
+sudo apt install imagemagick
+
+# pour le ssh
+sudo apt install openssh-server openssh-client
