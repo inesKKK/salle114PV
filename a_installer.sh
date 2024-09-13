8217b67c469cf5f1d2d5db8cbbbd01366e4c6066
JulesBDF <109226140+JulesBDF@users.noreply.github.com>
2024-09-13 11:47:45 +0200

    Option -y et maj install

--- a_installer.sh
+++ a_installer.sh
@@ -1,18 +1,23 @@
-sudo apt install emacs
-sudo apt install tuareg-mode
-sudo apt install utop
-sudo apt install gcc
-sudo apt install gdb
-sudo apt install postgresql
+sudo apt install -y emacs
+sudo apt install -y  tuareg-mode
+sudo apt install -y utop
+sudo apt install -y gcc
+sudo apt install -y gdb
+sudo apt install -y postgresql
+sudo apt install -y spyder
+wget https://github.com/VSCodium/vscodium/releases/download/1.93.1.24256/codium_1.93.1.24256_amd64.deb
+sudo apt install ./codium_1.93.1.24256_amd64.deb
+rm codium_1.93.1.24256_amd64.deb
+sudo apt install -y opam
 
 # pour les TP MP2I
-sudo apt install tree
-sudo apt install net-tools
-sudo apt install gnuplot
-sudo apt install imagemagick
+sudo apt install -y tree
+sudo apt install -y net-tools
+sudo apt install -y gnuplot
+sudo apt install -y imagemagick
 
 # pour le ssh
-sudo apt install openssh-server openssh-client
+sudo apt install -y openssh-server openssh-client
 
 # pour les TIPE
-sudo apt install libreoffice
+sudo apt install -y libreoffice
